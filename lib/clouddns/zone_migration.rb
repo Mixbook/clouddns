6c65eeb810946fd8ea9ff374c1df2dc54f6326d5
Robert Butler <robert@mixbook.com>
2014-05-29 17:50:31 -0700

    Need to compare sorted order for "sameness".

--- lib/clouddns/zone_migration.rb
+++ lib/clouddns/zone_migration.rb
@@ -72,7 +72,7 @@ module Clouddns
       # AWS replaces * with \052
       record.name == fog_record_name(fog_record) &&
         record.type == fog_record.type &&
-        record.value == fog_record.value &&
+        record.value.sort == fog_record.value.sort &&
         record.ttl.to_i == fog_record.ttl.to_i
     end
   end
